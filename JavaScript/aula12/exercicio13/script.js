d80060df9406548bd003e0ef8d1e2614f31741b9
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-10-06 16:27:46 -0300

    JavaScript - Aula12 - Exercicio13 different solution

--- JavaScript/aula12/exercicio13/script.js
+++ JavaScript/aula12/exercicio13/script.js
@@ -4,3 +4,15 @@
     palavraSeparada = palavraSeparada.join("");
     console.log(palavraSeparada);
 })('Gustavo');
+
+// Solução do professor.
+
+let palavra = "Hulk";
+
+function invertePalavra(p="Gustavo") {
+    for(let i=(p.length - 1); i >= 0; i--) {
+        console.log(p[i]);
+    }
+}
+
+invertePalavra('Diana');
