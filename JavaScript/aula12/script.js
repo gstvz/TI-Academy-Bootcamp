83b816ac7b6d2bde77c3fe1d0a9e09be3235e34e
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-10-06 14:34:11 -0300

    JavaScript - Aula12 - For loop

---
+++ JavaScript/aula12/script.js
@@ -0,0 +1,22 @@
+// incremento
+
+let n = 1;
+n++;
+n++;
+
+let frutas = ['Melancia', 'Banana', 'Uva', 'Nevascaranga'];
+frutas.push('Jabuticaba', 'Maçã');
+
+// console.log(frutas[n]);
+
+// for(let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
+
+// for (let i = frutas.length; i >= 0; i--) {
+//     console.log(frutas[i]);    
+// }
+
+// for (let i = frutas.length; i >= 0; i--) {
+//     console.log("JavaScript")  ;
+// }
