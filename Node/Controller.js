7f2c8472b99062d03dc78a101df5f5f3dc1ca46d
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-10-11 14:30:35 -0300

    Node - Aula6 and Aula7 - Implemented CREATE method for each route - Used POSTMAN for external requests

--- Node/Controller.js
+++ Node/Controller.js
@@ -1,23 +1,82 @@
 const express = require('express');
 const cors = require('cors');
+const models = require('./models');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+let cliente = models.Cliente;
+let itempedido = models.ItemPedido;
+let pedido = models.Pedido;
+let servico = models.Servico;
 
 app.get('/', function(req, res) {
     res.send('Olá, mundo!');
 });
 
-app.get('/clientes', function(req, res) {
-    res.send('Seja bem-vindo(a) à ServicesTI!');
+app.post('/clientes', async(req, res) => {
+    await cliente.create(
+        req.body        
+    ).then(function() {
+        return res.json({
+            error: false,
+            message: "Cliente criado com sucesso!"
+        });
+    }).catch(function(erro) {
+        return res.status(400).json({
+            error: true,
+            message: "Foi impossível se conectar."
+        })
+    });
 });
 
-app.get('/servicos', function(req, res) {
-    res.send('Seja bem-vindo(a) à área de Serviços!');
+app.post('/servicos', async(req, res) => {
+    await servico.create(
+        req.body        
+    ).then(function() {
+        return res.json({
+            error: false,
+            message: "Serviço criado com sucesso!"
+        });
+    }).catch(function(erro) {
+        return res.status(400).json({
+            error: true,
+            message: "Foi impossível se conectar."
+        })
+    });    
 });
 
-app.get('/pedidos', function(req, res) {
-    res.send('Seja bem-vindo(a) à área de Pedidos');
+app.post('/pedidos', async(req, res) => {
+    await pedido.create(
+        req.body
+    ).then(function() {
+        return res.json({
+            error: false,
+            message: "Pedido criado com sucesso!"
+        });
+    }).catch(function(erro) {
+        return res.status(400).json({
+            error: true,
+            message: "Foi impossível se conectar."
+        })
+    });
+});
+
+app.post('/itenspedido', async(req, res) => {
+    await itempedido.create(
+        req.body
+    ).then(function() {
+        return res.json({
+            error: false,
+            message: "Item adicionado ao pedido com sucesso!"
+        });
+    }).catch(function(erro) {
+        return res.status(400).json({
+            error: true,
+            message: "Foi impossível se conectar."
+        })
+    });
 });
 
 let port = process.env.PORT || 3001;
