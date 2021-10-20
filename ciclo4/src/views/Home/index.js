b043883414699383fe8ed843141077d6afc59782
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-10-20 13:31:51 -0300

    React - Aula3 and Aula4 - Created listar-servico Page - Applied margin to Home buttons

--- ciclo4/src/views/Home/index.js
+++ ciclo4/src/views/Home/index.js
@@ -6,28 +6,28 @@ export const Home = () => {
             <Container>
                 <div className="d-flex">
                     <div className="m-auto p-2">
-                        <h1>Home</h1>  
+                        <h1>Home</h1>
                     </div>
-                </div>
-                <div className="p-2">
-                    <a 
-                        href="/listar-cliente" 
-                        className="btn btn-outline-success btn-sm"
-                    >
-                        Cliente
-                    </a>
-                    <a 
-                        href="/listar-servico" 
-                        className="btn btn-outline-success btn-sm"
-                    >
-                        Serviço
-                    </a>
-                    <a 
-                        href="/listar-pedido" 
-                        className="btn btn-outline-success btn-sm"
-                    >
-                        Pedido
-                    </a>
+                    <div className="p-2">
+                        <a
+                            href="/listar-cliente"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Cliente
+                        </a>
+                        <a
+                            href="/listar-servico"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Serviço
+                        </a>
+                        <a
+                            href="/listar-pedido"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Pedido
+                        </a>
+                    </div>
                 </div>
             </Container>
         </div>
--- ciclo4/src/views/Servico/ListarServico/index.js
+++ ciclo4/src/views/Servico/ListarServico/index.js
@@ -1,7 +1,50 @@
+import axios from "axios";
+import { api } from "../../../config";
+import { Container, Table } from "reactstrap";
+import { useEffect, useState } from "react";
+
 export const ListarServico = () => {
+
+    const [data, setData] = useState([]);
+
+    const getServicos = async () => {
+        await axios.get(api + "/servicos/lista")
+            .then((response) => {
+                console.log(response.data.servicos);
+                setData(response.data.servicos);
+            })
+            .catch(() => {
+                console.log("Erro: sem conexão com a API.");
+            });
+    };
+
+    useEffect(() => {
+        getServicos();
+    }, []);
+
     return (
         <div>
-            Listar serviço
+            <Container>
+                <h1>Visualizar informações do serviço</h1>
+                <Table striped>
+                    <thead>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Ação</th>
+                    </thead>
+                    <tbody>
+                        {data.map(item => (
+                            <tr key={item.id}>
+                                <th>{item.id}</th>
+                                <td>{item.nome}</td>
+                                <td>{item.descricao}</td>
+                                <td className="text-center/">Botão</td>
+                            </tr>
+                        ))};
+                    </tbody>
+                </Table>
+            </Container>
         </div>
     );
 };
