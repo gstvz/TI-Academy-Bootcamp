a70b8f356214929ceb62c72ecea26c318c2bac93
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-10-20 18:37:13 -0300

    Node - Desafio - Inserted Cliente relation to Compra on Cliente model

--- Node/models/cliente.js
+++ Node/models/cliente.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Cliente.hasMany(models.Pedido, {foreignKey: 'ClienteId', as: 'pedidos'});
+      Cliente.hasMany(models.Compra, {foreignKey: 'ClienteId', as: 'compras'});
     }
   };
   Cliente.init({
