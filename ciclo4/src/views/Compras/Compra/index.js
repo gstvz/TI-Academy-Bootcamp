8038ac40f57f2025fdaca8065f1a721d38993274
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-11-05 15:47:31 -0300

    React - Compra: Changed ID to Produto ID

--- ciclo4/src/views/Compras/Compra/index.js
+++ ciclo4/src/views/Compras/Compra/index.js
@@ -60,7 +60,7 @@ export const Compra = (props) => {
                 <Table striped>
                     <thead>
                         <tr>
-                            <th>ID</th>
+                            <th>Produto ID</th>
                             <th className="text-center">Nome</th>
                             <th className="text-center">Descrição</th>                            
                             <th className="text-center">Ação</th>
