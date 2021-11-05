cebc060f22d60f3bb0db3018519286a442bbbda8
Gustavo França Albano de Paula <gstvz.franca@gmail.com>
2021-11-05 16:16:43 -0300

    React - Home: Changed Compras case - Changed Links to column

--- ciclo4/src/views/Home/index.js
+++ ciclo4/src/views/Home/index.js
@@ -8,54 +8,50 @@ export const Home = () => {
                     <div className="m-auto p-2">
                         <h1>Home</h1>
                     </div>
-                    <div>
-                        <div>
-                            <a
-                                href="/clientes"
-                                className="btn btn-outline-success btn-sm m-2"
-                            >
-                                Clientes
-                            </a>
-                            <a
-                                href="/pedidos"
-                                className="btn btn-outline-success btn-sm m-2"
-                            >
-                                Pedidos
+                    <div className="d-flex flex-column">                        
+                        <a
+                            href="/clientes"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Clientes
+                        </a>
+                        <a
+                            href="/pedidos"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Pedidos
 
-                            </a>
-                            <a
-                                href="/servicos"
-                                className="btn btn-outline-success btn-sm m-2"
-                            >
-                                Serviços
-                            </a>
-                            <a
-                                href="/itenspedidos"
-                                className="btn btn-outline-success btn-sm m-2"
-                            >
-                                Itens dos Pedidos
-                            </a>
-                        </div>
-                        <div>
-                            <a
-                                href="/compras"
-                                className="btn btn-outline-success btn-sm m-2"
-                            >
-                                Compras
-                            </a>
-                            <a
-                                href="/produtos"
-                                className="btn btn-outline-success btn-sm m-2"
-                            >
-                                Produtos
-                            </a>
-                            <a
-                                href="/itenscompras"
-                                className="btn btn-outline-success btn-sm m-2"
-                            >
-                                Itens das compras
-                            </a>
-                        </div>
+                        </a>
+                        <a
+                            href="/servicos"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Serviços
+                        </a>
+                        <a
+                            href="/itenspedidos"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Itens dos Pedidos
+                        </a>                  
+                        <a
+                            href="/compras"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Compras
+                        </a>
+                        <a
+                            href="/produtos"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Produtos
+                        </a>
+                        <a
+                            href="/itenscompras"
+                            className="btn btn-outline-success btn-sm m-2"
+                        >
+                            Itens das Compras
+                        </a>                        
                     </div>
                 </div>
             </Container>
